bb96364779dc6bf5664994cc2fdf69545f930f17
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2016-11-22 11:46:23 -0600

    Refactor main.js

--- public/main.js
+++ public/main.js
@@ -1,22 +1,63 @@
+//libs
 var two;
-var keyHeight;
-var queue = [];
-var queueId = [];
+//key height used to draw
+var game = {
+	keyHeight: 0,
+	keyWidth: 0,
+	keyIndexQueue: [],
+	rowQueue: [],
+	score: 0,
+	synth: null
+};
+
 var keyWasReleased = true;
-var score = 0;
-var synth;
-var notes = ['C', 'D', 'E', 'F#', 'G', 'A', 'B'];
 
-window.onload = function() {
-	synth = new Tone.Synth().toMaster();
-	two = new Two({
-		fullscreen: true
-	}).appendTo(document.querySelector('#draw-shapes'));
+const NOTES = ['C', 'D', 'E', 'F#', 'G', 'A', 'B'];
+const KEYS_PER_ROW = 4;
+const KEY_PRESSED_FILL = 'red';
+let rowsInScreen = 4;
 
-	keyHeight = two.height / 4;
+makeRow = function(y) {
+	game.keyWidth = two.width / KEYS_PER_ROW;
+	//puts the key n times keyHeight from the top. Because y position is measured in the center,
+	//so half the height is added.
 
-	var rows = [makeRow(0), makeRow(1), makeRow(2), makeRow(3), makeRow(4)];
-	var scoreText = new Two.Text(0, two.width / 2, 100, {
+	let reativeKeyX = (n) => {
+		return (n * game.keyWidth) + (game.keyWidth / 2);
+	}
+
+	// nth - 1 row of screen + half the height of the key.
+	let rowY =  (y - 1) * game.keyHeight + (.5 * game.keyHeight);
+
+	let key1 = two.makeRectangle(reativeKeyX(0), rowY, game.keyWidth,  game.keyHeight);
+	var key2 = two.makeRectangle(reativeKeyX(1), rowY,  game.keyWidth,  game.keyHeight);
+	var key3 = two.makeRectangle(reativeKeyX(2), rowY,  game.keyWidth,  game.keyHeight);
+	var key4 = two.makeRectangle(reativeKeyX(3), rowY,  game.keyWidth,  game.keyHeight);
+
+	let group = two.makeGroup(key1, key2, key3, key4);
+
+	group.stroke = '#BBB';
+	group.fill = '#FFFFFF';
+	group.linewidth = 1;
+
+	return group;
+}
+
+window.addEventListener('load', function() {
+	const TEXT_POSITION_Y = 100;
+	let rows = [];
+	//init synth
+	game.synth = new Tone.Synth().toMaster();
+	//init two
+	two = new Two({fullscreen: true}).appendTo(document.querySelector('#draw-shapes'));
+	game.keyHeight = two.height / rowsInScreen;
+
+	//add an extra row to appear offscreen
+	for (let i = 0; i < rowsInScreen + 1; i++) {
+		rows.push(makeRow(i));
+	}
+
+	let scoreText = new Two.Text(0, two.width / 2, TEXT_POSITION_Y, {
 		size: 100,
 		fill: '#fff',
 		stroke: '#000',
@@ -25,69 +66,59 @@ window.onload = function() {
 	two.add(scoreText);
 
 	two.bind('update', function(frameCount) {
-		scoreText.value = score;
+		scoreText.value = game.score;
 		for (let i = 0; i < rows.length; i++) {
 			refreshRow(rows[i], i);
 		}
 	}).play();
-}
+});
 
-
-makeRow = function(y) {
-	var keyWidth = two.width / 4;
-
-	var y = (y * keyHeight) + keyHeight / 2;
-
-	var key1 = two.makeRectangle(keyWidth / 2, y, keyWidth, keyHeight);
-	var key2 = two.makeRectangle(keyWidth + keyWidth / 2, y, keyWidth, keyHeight);
-	var key3 = two.makeRectangle(keyWidth * 2 + keyWidth / 2, y, keyWidth, keyHeight);
-	var key4 = two.makeRectangle(keyWidth * 3 + keyWidth / 2, y, keyWidth, keyHeight);
-
-	let group = two.makeGroup(key1, key2, key3, key4);
-	group.stroke = '#BBB';
-	group.fill = '#FFFFFF';
-	group.linewidth = 1;
-	return group;
-}
-
-refreshRow = function(row, offset) {
-	let ydif = score / 10 + 4;
+refreshRow = function(row, rowNumber) {
+	const INITIAL_LEVEL = 5;
+	let ydif =  INITIAL_LEVEL + game.score / 10;
 
 	var rowY = row.getBoundingClientRect().bottom;
-	if (rowY > two.height + keyHeight) {
-		row.translation.y = -(1 + offset) * keyHeight;
+
+	let yLimit = two.height + game.keyHeight;
+	let keyY = rowNumber * game.keyHeight;
+
+	let targetTranslationY =  0;
+
+	if (rowY > yLimit) {
+		row.translation.set(row.translation.x, -keyY);
 
 		for (let i = 0; i < row.children.length; i++)
 			row.children[i].fill = '#FFFFFF';
 
+
 		var blackKey = Math.floor(Math.random() * (4));
 		row.children[blackKey].fill = '#333333';
-		queue.push(blackKey);
-		queueId.push(row);
 
-		if (queueId.length === 5)
+		game.rowQueue.push(row);
+		game.keyIndexQueue.push(blackKey);
+
+		if (game.rowQueue.length === 5)
 		  two.pause();
 	}
 	row.translation.set(row.translation.x, row.translation.y + ydif);
-
 }
 
 check = function(i) {
-  if (queue[0] == i) {
-    var note = notes[Math.floor(Math.random() * (6))] + (Math.floor(Math.random() * (4)) + 3);
-    synth.triggerAttackRelease(note, "8n");
-    score++;
+  if (game.keyIndexQueue[0] == i) {
+    var note = NOTES[Math.floor(Math.random() * (6))] + (Math.floor(Math.random() * (4)) + 3);
+    game.synth.triggerAttackRelease(note, "8n");
+    game.score++;
 
-    var row = queueId[0];
-    row.children[queue[0]].fill = 'red';
-    queue.shift();
-    queueId.shift();
+    var row = game.rowQueue[0];
+    row.children[game.keyIndexQueue[0]].fill = KEY_PRESSED_FILL;
+    game.rowQueue.shift();
+    game.keyIndexQueue.shift();
 
   }
 }
 
 window.onkeydown = function(e) {
-	if (!keyWasReleased || queue[0] == undefined)
+	if (!keyWasReleased)
 		return;
 
 	var keys = {
@@ -107,13 +138,6 @@ window.onkeyup = function(e) {
 	keyWasReleased = true;
 }
 window.onclick = function(e) {
-  var w = two.width / 4;
-  if (e.clientX > w * 3)
-    check(3);
-  else if (e.clientX > w * 2)
-    check(2);
-  else if (e.clientX > w)
-    check(1);
-  else
-    check(0);
+	let screenSector = Math.floor(e.clientX / (two.width / 4));
+  check(screenSector);
 }
